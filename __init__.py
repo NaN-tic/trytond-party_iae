c60242a097f6ca9f18fa7a7cb5a80e77b1b35208
Albert Cervera i Areny <albert@nan-tic.com>
2020-12-05 00:43:50 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .party import *
+from . import party
 
 
 def register():
     Pool.register(
-        Iae,
-        Party,
-        PartyIae,
+        party.Iae,
+        party.Party,
+        party.PartyIae,
         module='party_iae', type_='model')
